9d96c2bb064f2588de6685042d34fa28cc703783
Oskar-Raczkowski <oskar.raczkowski99@gmail.com>
2022-01-17 18:46:39 +0100

    Add new files

---
+++ azurecred.py
@@ -0,0 +1,5 @@
+AZDBSERVER = 'michalmedynski.database.windows.net'
+AZDBNAME = 'michalmedynski' 
+AZDBUSER = 'michalmedynski' #twoja nazwa użytkownika
+AZDBPW = 'Michal28.07' #twoje hasło,którego nie powinniśmy znać
+AZDBDRIVER = '{ODBC Driver 17 for SQL Server}' 
